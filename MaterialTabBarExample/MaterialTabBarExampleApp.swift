a7109286d5170a81557cd9c5b51f2fbdec9772ec
Russell Gordon <rgordon@lcs.on.ca>
2023-11-09 23:15:24 -0500

    Initial Commit

---
+++ MaterialTabBarExample/MaterialTabBarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialTabBarExampleApp.swift
+//  MaterialTabBarExample
+//
+//  Created by Russell Gordon on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct MaterialTabBarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
